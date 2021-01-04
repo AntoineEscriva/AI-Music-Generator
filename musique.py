998028dce0064976eb25e12edbd399ade0c8dc99
Tuonick <florian.bossard@gmail.com>
2021-01-04 19:10:57 +0100

    J'arrete là pour ce soir

--- musique.py
+++ musique.py
@@ -23,6 +23,7 @@ class Musique(tkinter.Tk):
 		self.geometry("420x260")
 		self.switch_frame(Menu)
 		self.configure(bg='white')
+		self.font = tkinter.font.Font(size=55)
 
 	def switch_frame(self, frame_class):
 		newFrame = frame_class(self)
@@ -37,14 +38,15 @@ class Menu(tkinter.Frame):
 		
 	def __init__(self, master):
 		tkinter.Frame.__init__(self, master)
+		self.UneFont = "Helvetica, 10"
 
 		#---------Label------------#
 		self.configure(bg='white')
 		#titre
-		self.titre = tkinter.Label(self, text="Configuration", bg='white').grid(row=0,column=0)
+		self.titre = tkinter.Label(self, text="Configuration", bg='white', font ="Helvetica,30").grid(row=0,column=0)
 
 		#Label sélectionner un dossier
-		tkinter.Label(self, text="Sélectionner un dossier",height = hauteurBout, bg="white").grid(row = 1, column = 0, sticky="W")
+		tkinter.Label(self, text="Sélectionner un dossier",height = hauteurBout, bg="white", font =self.UneFont).grid(row = 1, column = 0, sticky="W")
 		
 		#Zone pour l'affichage du chemin
 		self.entry_text = tkinter.StringVar()
@@ -52,25 +54,25 @@ class Menu(tkinter.Frame):
 		self.usr_input.grid(row=1,column=1, sticky='EW')
 		
 		#Bouton ouvrir dossier
-		self.UF = tkinter.Button(self, text="Choisir", bg = "white", bd=1, width = largeurBout,command = lambda:[master.switch_frame(self.Browser())])
-		self.UF.grid(row = 1, column = 2, sticky="EW")
+		self.UF = tkinter.Button(self, text="Choisir", bg = "white", bd=1, width = largeurBout-5,command = lambda:[master.switch_frame(self.Browser())])
+		self.UF.grid(row = 1, column = 2, sticky="E")
 		
 		#Nombre de morceaux
-		tkinter.Label(self, text="Nombre de morceaux",height = hauteurBout, bg='white').grid(row = 2, column =0, sticky="W")
+		tkinter.Label(self, text="Nombre de morceaux",height = hauteurBout, bg='white', font =self.UneFont).grid(row = 2, column =0, sticky="W")
 		self.nbMorceaux = tkinter.Spinbox(self, from_=1, to=20).grid(row = 2, column =1, sticky="EW")
 
 		# Durée de morceaux
-		tkinter.Label(self, text="Durée de morceaux",height = hauteurBout, bg='white').grid(row = 3, column =0, sticky="W")
+		tkinter.Label(self, text="Durée de morceaux",height = hauteurBout, bg='white', font =self.UneFont).grid(row = 3, column =0, sticky="W")
 		
 		#Choix de génération
-		tkinter.Label(self, text="Choix de génération", width = 15, height = hauteurBout, bg='white').grid(row = 4, column =0, sticky="W")
+		tkinter.Label(self, text="Choix de génération", width = 15, height = hauteurBout, bg='white', font =self.UneFont).grid(row = 4, column =0, sticky="W")
 				
 		#Bouton nb morceau
 		self.dureeMorceau = tkinter.Spinbox(self, from_=60,to=340)
 		self.dureeMorceau.grid(row = 3, column =1, sticky="EW")
 		
 		#Bouton valider
-		self.Valider = tkinter.Button(self, text="Valider", bg="white", bd=1, command=lambda :[master.switch_frame(lecteur.Lecteur)])
+		self.Valider = tkinter.Button(self, text="Valider", bg="white",font =self.UneFont, bd=1, command=lambda :[master.switch_frame(lecteur.Lecteur)])
 		self.Valider.grid(row=5,column = 1,sticky="EW")
 		
 		#Bouton Choix generation	
