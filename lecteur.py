03823848a52d537b10c7656ae6900326693b897b
Tuonick <florian.bossard@gmail.com>
2021-01-04 20:48:03 +0100

    Filename OK

--- lecteur.py
+++ lecteur.py
@@ -22,8 +22,8 @@ class Lecteur(tkinter.Frame):
 		tkinter.Frame.__init__(self, master)
 		
 		self.configure(bg='white')
-		tkinter.Label(self, text="Résultat de la génération",height = hauteurBout, bg='white').grid(row = 0, column = 0, sticky="W", columnspan=2) 
-
+		tkinter.Label(self, text="Résultat de la génération",height = hauteurBout, bg='white',font ="Helvetica,30").grid(row = 0, column = 0, sticky="W", columnspan=2) 
+		
 		#Skip Left
 		self.skip_left = PhotoImage(file="./buttons_resize/skip_left.png")
 		self.skip_left_button = tkinter.Button(self)
@@ -50,34 +50,41 @@ class Lecteur(tkinter.Frame):
 		
 		#Some Labels
 		tkinter.Label(self, text="",height = hauteurBout,bg='white').grid(row = 2, column = 0, sticky="NESW") 
-		tkinter.Label(self, text="Télécharger",bg='white').grid(row = 2, column = 0, sticky="W") 
+		tkinter.Label(self, text="Télécharger",bg='white', font ="Helvetica").grid(row = 2, column = 0, sticky="W") 
 		tkinter.Label(self, text="Choix de l'extension :",height = hauteurBout,bg='white').grid(row = 3, column = 0, sticky="W") 
 		
 		#Combobox
 		self.extension = ttk.Combobox(self,state='readonly', values = [".midi",".wav"],width="5")
 		self.extension.current(0)
-		self.extension.grid(row=4, column=0, sticky = "w")
+		self.extension.grid(row=4, column=0, sticky = "EW")
+		
+		#Zone pour l'affichage du chemin
+		self.entry_text = tkinter.StringVar()
+		self.usr_input = ttk.Entry(self, state='readonly', textvariable=self.entry_text)
+		self.usr_input.grid(row=4,column=3, sticky='EW')
 		
 		#Sélection dossier
 		tkinter.Label(self, text="Sélectionner un dossier",height = hauteurBout, bg='white').grid(row = 3, column = 2, sticky="W", columnspan=2)
-		self.UF = tkinter.Button(self, bg='white', text="Choisir",command = lambda:[master.switch_frame(self.Browser())])
+		self.UF = tkinter.Button(self, bg='white', text="Choisir",command = lambda:[self.BrowserFile()])
 		self.UF.grid(row = 4, column = 2, sticky="W")
 
 		#Download
-		self.download = PhotoImage(file="./buttons_resize/DownloadButton.png")
-		self.download_button = tkinter.Button(self)
+		#self.download = PhotoImage(file="./buttons_resize/DownloadButton.png")
+		self.download_button = tkinter.Button(self, text="Télécharger")
 		self.download_button.grid(row = 4, column = 4, sticky="E")
-		self.download_button.config(image = self.download,bg='white')
+		#self.download_button.config(image = self.download,bg='white')
+		self.download_button.config(bg='white')
 		
 		#Bouton Retour
 		self.retour = tkinter.Button(self, text="Retour", command=lambda : [master.switch_frame(musique.Menu)])
-		self.retour.grid(row=7, column=0, sticky="WS")
+		tkinter.Label(self, text="  ", bg="white").grid(row=6, column=0)
+		
+		self.retour.grid(row=7,column=0, sticky="WS")
 		self.retour.config(bd=1, bg="white")
 	
-	def Browser(self):
+	def BrowserFile(self):
 		filename = filedialog.askdirectory(initialdir = "/")
-		new_text = "Ici le chemin"
-		self.entry_text.set(new_text)		
+		self.entry_text.set(filename)		
 	
 
 
--- musique.py
+++ musique.py
@@ -20,10 +20,10 @@ class Musique(tkinter.Tk):
 		tkinter.Tk.__init__(self)
 		self.frame = None
 		self.title("Génération musique aléatoire")
-		self.geometry("420x260")
+		self.geometry("470x255")
 		self.switch_frame(Menu)
 		self.configure(bg='white')
-		self.font = tkinter.font.Font(size=55)
+		#self.font = tkinter.font.Font(size=55)
 
 	def switch_frame(self, frame_class):
 		newFrame = frame_class(self)
@@ -43,10 +43,10 @@ class Menu(tkinter.Frame):
 		#---------Label------------#
 		self.configure(bg='white')
 		#titre
-		self.titre = tkinter.Label(self, text="Configuration", bg='white', font ="Helvetica,30").grid(row=0,column=0)
+		self.titre = tkinter.Label(self, text="Configuration", bg='white', font ="Helvetica").grid(row=0,column=0)
 
 		#Label sélectionner un dossier
-		tkinter.Label(self, text="Sélectionner un dossier",height = hauteurBout, bg="white", font =self.UneFont).grid(row = 1, column = 0, sticky="W")
+		tkinter.Label(self, text="Sélectionner un dossier",height = hauteurBout, bg="white").grid(row = 1, column = 0, sticky="W")
 		
 		#Zone pour l'affichage du chemin
 		self.entry_text = tkinter.StringVar()
@@ -54,25 +54,25 @@ class Menu(tkinter.Frame):
 		self.usr_input.grid(row=1,column=1, sticky='EW')
 		
 		#Bouton ouvrir dossier
-		self.UF = tkinter.Button(self, text="Choisir", bg = "white", bd=1, width = largeurBout-5,command = lambda:[master.switch_frame(self.Browser())])
+		self.UF = tkinter.Button(self, text="Choisir", bg = "white", bd=1, width = largeurBout-5,command = lambda:[self.Browser()])
 		self.UF.grid(row = 1, column = 2, sticky="E")
 		
 		#Nombre de morceaux
-		tkinter.Label(self, text="Nombre de morceaux",height = hauteurBout, bg='white', font =self.UneFont).grid(row = 2, column =0, sticky="W")
+		tkinter.Label(self, text="Nombre de morceaux",height = hauteurBout, bg='white').grid(row = 2, column =0, sticky="W")
 		self.nbMorceaux = tkinter.Spinbox(self, from_=1, to=20).grid(row = 2, column =1, sticky="EW")
 
 		# Durée de morceaux
-		tkinter.Label(self, text="Durée de morceaux",height = hauteurBout, bg='white', font =self.UneFont).grid(row = 3, column =0, sticky="W")
+		tkinter.Label(self, text="Durée de morceaux",height = hauteurBout, bg='white').grid(row = 3, column =0, sticky="W")
 		
 		#Choix de génération
-		tkinter.Label(self, text="Choix de génération", width = 15, height = hauteurBout, bg='white', font =self.UneFont).grid(row = 4, column =0, sticky="W")
+		tkinter.Label(self, text="Choix de génération", width = 15, height = hauteurBout, bg='white').grid(row = 4, column =0, sticky="W")
 				
 		#Bouton nb morceau
 		self.dureeMorceau = tkinter.Spinbox(self, from_=60,to=340)
 		self.dureeMorceau.grid(row = 3, column =1, sticky="EW")
 		
 		#Bouton valider
-		self.Valider = tkinter.Button(self, text="Valider", bg="white",font =self.UneFont, bd=1, command=lambda :[master.switch_frame(lecteur.Lecteur)])
+		self.Valider = tkinter.Button(self, text="Valider", bg="white", bd=1, command=lambda :[master.switch_frame(lecteur.Lecteur)])
 		self.Valider.grid(row=5,column = 1,sticky="EW")
 		
 		#Bouton Choix generation	
@@ -83,28 +83,9 @@ class Menu(tkinter.Frame):
 
 	def Browser(self):
 		filename = filedialog.askdirectory(initialdir = "/")
-		new_text = "Ici le chemin"
-		self.entry_text.set(new_text)
+		self.entry_text.set(filename)
 		
 ####################################################### 
-
-class UneFonction(tkinter.Frame):
-
-
-	def __init__(self, master):
-		print("coucou")
-		tkinter.Frame.__init__(self, master)
-		fontStyle = tkFont.Font( size=20)
-		tkinter.Label(self, text="Une Fonction", font = fontStyle ).grid(row = 0, column = 0, columnspan = 2)
-		self.valider = tkinter.Button(self, text = "Valider", width = largeurBout, height = hauteurBout,  command = lambda : [self.UneFonctionalite()]).grid(row = 2, column = 1, sticky = 'W')
-		self.result = tkinter.StringVar()
-		self.result_text = tkinter.Label(self, textvariable = self.result, justify = tkinter.LEFT)
-		self.result_text.grid(row = 3, column = 0, sticky = 'W')
-		self.result.set("Solution: ")
-
-	def UneFonctionalite(self):
-		self.result.set("Une phrase")
-
 
 ##Ci-dessous, deux fonctions copiées collées de Google pour centrer la fenêtre au milieu de l'écran
 
