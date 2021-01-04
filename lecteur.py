af38588a99cd65c0269d8e29465583be45c93e5d
Tuonick <florian.bossard@gmail.com>
2021-01-04 18:52:24 +0100

    Unification du fond
    Fond Blanc, et quelques corrections

--- lecteur.py
+++ lecteur.py
@@ -8,6 +8,8 @@ from tkinter import ttk
 from tkinter import *
 from PIL import Image,ImageTk
 
+import musique as musique
+
 largeurBout = 15
 hauteurBout = 2
 margeX = 0
@@ -19,37 +21,37 @@ class Lecteur(tkinter.Frame):
 	def __init__(self, master):
 		tkinter.Frame.__init__(self, master)
 		
-		#self.configure(bg='royalblue')
-		tkinter.Label(self, text="Résultat de la génération",height = hauteurBout).grid(row = 0, column = 0, sticky="W", columnspan=2) 
+		self.configure(bg='white')
+		tkinter.Label(self, text="Résultat de la génération",height = hauteurBout, bg='white').grid(row = 0, column = 0, sticky="W", columnspan=2) 
 
 		#Skip Left
 		self.skip_left = PhotoImage(file="./buttons_resize/skip_left.png")
 		self.skip_left_button = tkinter.Button(self)
 		self.skip_left_button.grid(row = 1, column = 0, sticky="EW")
-		self.skip_left_button.config(image = self.skip_left)
+		self.skip_left_button.config(image = self.skip_left, bd=0,bg='white')
 		
 		#Play
 		self.play = PhotoImage(file="./buttons_resize/play.png")
 		self.play_button = tkinter.Button(self)
-		self.play_button.grid(row = 1, column = 1, sticky="EW")
-		self.play_button.config(image = self.play)
+		self.play_button.grid(row = 1, column = 2, sticky="E")
+		self.play_button.config(image = self.play, bd=0,bg='white')
 		
 		#Pause
 		self.pause = PhotoImage(file="./buttons_resize/pause.png")
 		self.pause_button = tkinter.Button(self)
-		self.pause_button.grid(row = 1, column = 2, sticky="EW")
-		self.pause_button.config(image = self.pause)
+		self.pause_button.grid(row = 1, column = 3, sticky="EW")
+		self.pause_button.config(image = self.pause, bd=0, bg='white')
 		
 		#Skip Right
 		self.skip_right = PhotoImage(file="./buttons_resize/skip_right.png")
 		self.skip_right_button = tkinter.Button(self)
-		self.skip_right_button.grid(row = 1, column = 3, sticky="EW")
-		self.skip_right_button.config(image = self.skip_right)
+		self.skip_right_button.grid(row = 1, column = 4, sticky="E")
+		self.skip_right_button.config(image = self.skip_right, bd=0,bg='white')
 		
 		#Some Labels
-		tkinter.Label(self, text="",height = hauteurBout).grid(row = 2, column = 0, sticky="NESW") 
-		tkinter.Label(self, text="Télécharger").grid(row = 2, column = 0, sticky="W") 
-		tkinter.Label(self, text="Choix de l'extension :",height = hauteurBout).grid(row = 3, column = 0, sticky="W") 
+		tkinter.Label(self, text="",height = hauteurBout,bg='white').grid(row = 2, column = 0, sticky="NESW") 
+		tkinter.Label(self, text="Télécharger",bg='white').grid(row = 2, column = 0, sticky="W") 
+		tkinter.Label(self, text="Choix de l'extension :",height = hauteurBout,bg='white').grid(row = 3, column = 0, sticky="W") 
 		
 		#Combobox
 		self.extension = ttk.Combobox(self,state='readonly', values = [".midi",".wav"],width="5")
@@ -57,15 +59,20 @@ class Lecteur(tkinter.Frame):
 		self.extension.grid(row=4, column=0, sticky = "w")
 		
 		#Sélection dossier
-		tkinter.Label(self, text="Sélectionner un dossier",height = hauteurBout).grid(row = 3, column = 3, sticky="W", columnspan=2)
-		self.UF = tkinter.Button(self, text="Choisir",command = lambda:[master.switch_frame(self.Browser())])
-		self.UF.grid(row = 4, column = 3, sticky="W")
+		tkinter.Label(self, text="Sélectionner un dossier",height = hauteurBout, bg='white').grid(row = 3, column = 2, sticky="W", columnspan=2)
+		self.UF = tkinter.Button(self, bg='white', text="Choisir",command = lambda:[master.switch_frame(self.Browser())])
+		self.UF.grid(row = 4, column = 2, sticky="W")
 
 		#Download
 		self.download = PhotoImage(file="./buttons_resize/DownloadButton.png")
 		self.download_button = tkinter.Button(self)
-		self.download_button.grid(row = 5, column = 1, sticky="EW")
-		self.download_button.config(image = self.download)
+		self.download_button.grid(row = 4, column = 4, sticky="E")
+		self.download_button.config(image = self.download,bg='white')
+		
+		#Bouton Retour
+		self.retour = tkinter.Button(self, text="Retour", command=lambda : [master.switch_frame(musique.Menu)])
+		self.retour.grid(row=7, column=0, sticky="WS")
+		self.retour.config(bd=1, bg="white")
 	
 	def Browser(self):
 		filename = filedialog.askdirectory(initialdir = "/")
--- musique.py
+++ musique.py
@@ -20,15 +20,16 @@ class Musique(tkinter.Tk):
 		tkinter.Tk.__init__(self)
 		self.frame = None
 		self.title("Génération musique aléatoire")
-		self.geometry("480x300")
+		self.geometry("420x260")
 		self.switch_frame(Menu)
+		self.configure(bg='white')
 
 	def switch_frame(self, frame_class):
 		newFrame = frame_class(self)
 		if(self.frame is not None):
 			self.frame.grid_remove()
 		self.frame = newFrame
-		self.frame.grid(padx = 20, pady = 20, sticky = "ewsn")
+		self.frame.grid(padx = 10, pady = 10, sticky = "ewsn")
 		
 #######################################################
 
@@ -38,41 +39,44 @@ class Menu(tkinter.Frame):
 		tkinter.Frame.__init__(self, master)
 
 		#---------Label------------#
+		self.configure(bg='white')
+		#titre
+		self.titre = tkinter.Label(self, text="Configuration", bg='white').grid(row=0,column=0)
+
+		#Label sélectionner un dossier
+		tkinter.Label(self, text="Sélectionner un dossier",height = hauteurBout, bg="white").grid(row = 1, column = 0, sticky="W")
 		
 		#Zone pour l'affichage du chemin
 		self.entry_text = tkinter.StringVar()
-		self.usr_input = ttk.Entry(self,state='readonly',textvariable=self.entry_text)
-		self.usr_input.grid(row=0,column=1, sticky='EW')
-
-		#Label sélectionner un dossier
-		tkinter.Label(self, text="Sélectionner un dossier",height = hauteurBout).grid(row = 0, column = 0, sticky="W")
+		self.usr_input = ttk.Entry(self, state='readonly', textvariable=self.entry_text)
+		self.usr_input.grid(row=1,column=1, sticky='EW')
 		
 		#Bouton ouvrir dossier
-		self.UF = tkinter.Button(self, text="Choisir", bg = "white", width = largeurBout,command = lambda:[master.switch_frame(self.Browser())])
-		self.UF.grid(row = 0, column = 2, sticky="EW")
+		self.UF = tkinter.Button(self, text="Choisir", bg = "white", bd=1, width = largeurBout,command = lambda:[master.switch_frame(self.Browser())])
+		self.UF.grid(row = 1, column = 2, sticky="EW")
 		
 		#Nombre de morceaux
-		tkinter.Label(self, text="Nombre de morceaux",height = hauteurBout).grid(row = 1, column =0, sticky="W")
-		self.nbMorceaux = tkinter.Spinbox(self, from_=1, to=20).grid(row = 1, column =1, sticky="EW")
+		tkinter.Label(self, text="Nombre de morceaux",height = hauteurBout, bg='white').grid(row = 2, column =0, sticky="W")
+		self.nbMorceaux = tkinter.Spinbox(self, from_=1, to=20).grid(row = 2, column =1, sticky="EW")
 
 		# Durée de morceaux
-		tkinter.Label(self, text="Durée de morceaux",height = hauteurBout).grid(row = 2, column =0, sticky="W")
+		tkinter.Label(self, text="Durée de morceaux",height = hauteurBout, bg='white').grid(row = 3, column =0, sticky="W")
 		
 		#Choix de génération
-		tkinter.Label(self, text="Choix de génération", width = 15, height = hauteurBout).grid(row = 3, column =0, sticky="W")
+		tkinter.Label(self, text="Choix de génération", width = 15, height = hauteurBout, bg='white').grid(row = 4, column =0, sticky="W")
 				
 		#Bouton nb morceau
 		self.dureeMorceau = tkinter.Spinbox(self, from_=60,to=340)
-		self.dureeMorceau.grid(row = 2, column =1, sticky="EW")
+		self.dureeMorceau.grid(row = 3, column =1, sticky="EW")
 		
 		#Bouton valider
-		self.Valider = tkinter.Button(self, text="Valider", bg = "white",  command=lambda :[master.switch_frame(lecteur.Lecteur)])
+		self.Valider = tkinter.Button(self, text="Valider", bg="white", bd=1, command=lambda :[master.switch_frame(lecteur.Lecteur)])
 		self.Valider.grid(row=5,column = 1,sticky="EW")
 		
 		#Bouton Choix generation	
-		self.comboboite = ttk.Combobox(self, values = ["Rythme seulement","Rythme et mélodie","Polyphonie"])
+		self.comboboite = ttk.Combobox(self,values = ["Rythme seulement","Rythme et mélodie","Polyphonie"])
 		self.comboboite.current(0)
-		self.comboboite.grid(row=3, column=1)
+		self.comboboite.grid(row=4, column=1)
 		
 
 	def Browser(self):
@@ -100,10 +104,24 @@ class UneFonction(tkinter.Frame):
 		self.result.set("Une phrase")
 
 
+##Ci-dessous, deux fonctions copiées collées de Google pour centrer la fenêtre au milieu de l'écran
+
+def geoliste(g):
+    r=[i for i in range(0,len(g)) if not g[i].isdigit()]
+    return [int(g[0:r[0]]),int(g[r[0]+1:r[1]]),int(g[r[1]+1:r[2]]),int(g[r[2]+1:])]
+
+def centrefenetre(fen):
+    fen.update_idletasks()
+    l,h,x,y=geoliste(fen.geometry())
+    fen.geometry("%dx%d%+d%+d" % (l,h,(fen.winfo_screenwidth()-l)//2,(fen.winfo_screenheight()-h)//2))
+ 
+# exemple d'utilisation:
 
 if __name__ == "__main__":
 
-	app = Musique()	
+	app = Musique()
+	centrefenetre(app)
+	app.resizable(width=False, height=False)
 	app.mainloop()
 	
 
