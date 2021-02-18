a1ecd1eebf7d4560f9d9cee5cbcb06f1ade7eb98
Hvedrug <63800678+Hvedrug@users.noreply.github.com>
2021-02-18 19:18:40 +0100

    Update exportParam.py
    modifications pour enregistrer en csv au lieu de txt, le code pour enregistrer en txt est toujours présent en commentaire.

--- files/scripts/exportParam.py
+++ files/scripts/exportParam.py
@@ -2,11 +2,18 @@
 #coding: utf-8
 
 
-def export(parametres):
-	fichier = open("params.txt", "w")
+def export(parametres, filename="params.csv"):
+	'''
+	fichier = open(filename, "w")
 	
 	fichier.write(str(parametres))
 	
 	fichier.close()
+	'''
+	
+	with open(filename, 'w') as f:
+		w = csv.DictWriter(f, parametres.keys())
+		w.writeheader()
+		w.writerow(parametres)
 	print("Fichier de paramètres exporté")
 	return
