a866d17e1ba7e42e692ba69878a9c378c7f9eac8
Tuonick <florian.bossard@gmail.com>
2021-03-11 15:42:38 +0100

    Ajout fonction verification

--- files/lecteur.py
+++ files/lecteur.py
@@ -37,7 +37,13 @@ class Lecteur(tkinter.Frame):
 		self.numeroTitre = 0
 		self.listeMusiques = []
 		#Titre
-		tkinter.Label(self, text="Résultat de la génération", font = self.titre, height = hauteurBout, bg='white').grid(row = 0, column = 0, sticky="W", columnspan=2) 
+		tkinter.Label(self, text="Résultat de la génération", font = self.titre, height = hauteurBout, bg='white').grid(row = 0, column = 0, sticky="W") 
+		#Titre de la musique en cours
+		self.titreMusique = ""
+		self.boxTitreMusique = tkinter.Label(self, text="Titre: ", bg='white', font = self.texte).grid(row=1,column=0, sticky="w")
+		
+		#Espace
+		#tkinter.Label(self, text="",height = hauteurBout,bg='white').grid(row = 2, column = 0) 
 		
 		#Bouton Skip Left
 		#Affectation d'une image de bouton à une variable
@@ -45,9 +51,10 @@ class Lecteur(tkinter.Frame):
 		#Création du bouton
 		self.skip_left_button = tkinter.Button(self)
 		#Placement du bouton
-		self.skip_left_button.grid(row = 2, column = 0, sticky="W")
+		self.skip_left_button.grid(row = 3, column = 0, sticky="W")
 		#Quelques réglages d'apparence : couleur du background, épaisseur des bordures, et affectation de l'image au bouton
 		self.skip_left_button.config(image = self.skip_left, bd=0,bg='white', command= lambda:[self.previousSong()])
+		self.grid(row=3, column=0, pady=(100, 10))
 		
 		#Bouton Play
 		#Ajout d'un compteur au bouton play
@@ -57,7 +64,7 @@ class Lecteur(tkinter.Frame):
 		#Création du bouton
 		self.play_button = tkinter.Button(self)
 		#Placement du bouton
-		self.play_button.grid(row = 2, column = 0)
+		self.play_button.grid(row = 3, column = 0)
 		#Quelques réglages d'apparence : couleur du background, épaisseur des bordures, et affectation de l'image au bouton
 		self.play_button.config(image = self.play, bd=0,bg='white', command= lambda: [self.playUnPause()])
 		
@@ -68,7 +75,7 @@ class Lecteur(tkinter.Frame):
 		#Création du bouton
 		self.pause_button = tkinter.Button(self)
 		#Placement du bouton
-		self.pause_button.grid(row = 2, column =0, sticky="E")
+		self.pause_button.grid(row = 3, column =0, sticky="e")
 		#Quelques réglages d'apparence : couleur du background, épaisseur des bordures, et affectation de l'image au bouton
 		self.pause_button.config(image = self.pause, bd=0, bg='white', command = lambda: [pygame.mixer.music.pause()])
 		
@@ -78,42 +85,38 @@ class Lecteur(tkinter.Frame):
 		#Création du bouton
 		self.skip_right_button = tkinter.Button(self)
 		#Placement du bouton
-		self.skip_right_button.grid(row = 2, column = 1)
+		self.skip_right_button.grid(row = 3, column = 2)
 		#Quelques réglages d'apparence : couleur du background, épaisseur des bordures, et affectation de l'image au bouton
 		self.skip_right_button.config(image = self.skip_right, bd=0,bg='white', command = lambda:[self.nextSong()])
 		
-		#Labels
-		#Titre de la musique en cours
-		self.titreMusique = tkinter.StringVar()
-		self.boxTitreMusique = tkinter.Label(self, textvariable=self.titreMusique, bg='white', font = self.texte).grid(row=1,column=0, sticky = "W")
-		
-		tkinter.Label(self, text="",height = hauteurBout,bg='white').grid(row = 3, column = 0, sticky="NESW") 
+
+		#tkinter.Label(self, text="",height = hauteurBout,bg='white').grid(row = 4, column = 0) 
 		
 		#Création et placement du Label Enregistrement, avec background blanc
-		tkinter.Label(self, text="Enregistrement",bg='white', font = self.titre).grid(row = 4, column = 0, sticky="W") 
+		tkinter.Label(self, text="Enregistrement",bg='white', font = self.titre).grid(row = 5, column = 0, sticky="W") 
 		#Création et placement du label Choix de l'Extension
-		tkinter.Label(self, text="Choix de l'extension :", font = self.texte, height = hauteurBout,bg='white').grid(row = 5, column = 0, sticky="W") 
+		tkinter.Label(self, text="Choix de l'extension :", font = self.texte, height = hauteurBout,bg='white').grid(row = 6, column = 0, sticky="W") 
 		
 		#Création d'une Combobox pour le choix de l'extension
-		self.extension = ttk.Combobox(self,state='readonly', values = [".midi",".wav"],width="5")
+		self.extension = ttk.Combobox(self,state='readonly', values = [".midi",".wav"])
 		#Selection de l'item par défaut
 		self.extension.current(0)
 		#Placement
-		self.extension.grid(row=6, column=0, sticky = "EW")
+		self.extension.grid(row=7, column=0, sticky = "W")
 		
 		#Bouton Enregistrement
 		#Création du bouton, relié à une fonction de sélection du chemin d'enregistrement (FNC_selection)
 		self.download_button = tkinter.Button(self, text="Supprimer les fichiers", font = self.texte,  command = lambda:[self.supprimerFichiers()])
 		#Placement
-		self.download_button.grid(row = 6, column =2, sticky="E")
+		self.download_button.grid(row = 7, column =2, sticky="W")
 		#Réglage du background
-		self.download_button.config(bg='white')
+		self.download_button.config(bg='white', bd=1)
 			
 		#Bouton Retour
 		#Creation du bouton retour, relié la classe menu 
 		self.retour = tkinter.Button(self, text="Retour", font = self.texte, command=lambda : [pygame.mixer.music.pause(),master.switch_frame(musique.Menu)])
 		#Placement
-		self.retour.grid(row=8,column=0, sticky="WS")
+		self.retour.grid(row=9,column=0, sticky="WS")
 		#Réglage de la bordure du bouton et du background
 		self.retour.config(bd=1, bg="white")
 		
@@ -126,13 +129,6 @@ class Lecteur(tkinter.Frame):
 		#Initialisation des musiques lecteur 
 		self.initLecteur()
 		
-	def playUnPause(self):
-		if(self.comptePlay ==0):
-			pygame.mixer.music.play()
-			self.comptePlay = 1
-		else:
-			pygame.mixer.music.unpause()
-		return 
 		
 	def supprimerFichiers(self):
 		print("Code effectif à écrire")
@@ -167,10 +163,17 @@ class Lecteur(tkinter.Frame):
 			pygame.mixer.music.load("./files/midi/"+str(self.listeMusiques[0]))	#Charge le premier titre
 		except pygame.error:
 			print("Echec de chargement du fichier midi\n")
-		self.titreMusique.set("Titre: ")#+self.listeMusiques[0]
-		
 		self.comboTitres["values"]=self.listeMusiques
 		self.comboTitres.current(0)
+	
+	
+	def playUnPause(self):
+		if(self.comptePlay ==0):
+			pygame.mixer.music.play()
+			self.comptePlay = 1
+		else:
+			pygame.mixer.music.unpause()
+		return 
 	
 	def nextSong(self):
 		if self.comptePlay == 1: #Si c'est 1 c'est qu'un son est joué
@@ -202,7 +205,6 @@ class Lecteur(tkinter.Frame):
 		pygame.mixer.music.load("./files/midi/"+self.comboTitres.get()) #Charge le titre 
 		pygame.mixer.music.play() # play the song var corresponds to
 		self.numeroTitre = self.comboTitres.current() #On met à jour le numero de titre joué
-
 		
 		
 
--- files/musique.py
+++ files/musique.py
@@ -32,7 +32,7 @@ class Musique(tkinter.Tk):
 		#Affectation du titre de la fenêtre
 		self.title("Génération musique aléatoire")
 		#Réglage de la taille de la fenêtre
-		self.geometry("515x420")
+		self.geometry("535x420")
 		#Réglage du fond en blanc
 		self.configure(bg='white')
 		#Appel de la methode switch_frame qui se situe ci-dessous
@@ -72,53 +72,58 @@ class Menu(tkinter.Frame):
 		#Création et placement du titre du cadre
 		self.titre = tkinter.Label(self, text="Configuration", bg='white', font = self.titre).grid(row=0,column=0)
 
-		#Création et placement du label "sélectionner un dossier"
-		tkinter.Label(self, text="Sélectionner un dossier",height = hauteurBout, bg="white", font = self.texte).grid(row = 1, column = 0, sticky="W")
-		
 		#Création de la Zone d'affichage du chemin
 		self.entry_text = tkinter.StringVar()
-		self.cheminAbsolu = os.getcwd()
-		self.entry_text.set(self.cheminAbsolu+"/files/midi")
+		self.entry_text.set(os.getcwd()+"/files/midi")
+		
+		#Traitement affichage chemin
+		self.affichageChemin = tkinter.StringVar()
+		self.affichageChemin.set(self.traitementAffichage(self.entry_text.get(),29))
+		
 		#Placement de la zone et affectation d'un texte dans cette zone d'affichage
-		self.usr_input = ttk.Entry(self, state='readonly', textvariable=self.entry_text).grid(row=1,column=1, sticky='EW')
+		self.usr_input = tkinter.Entry(self, state='readonly', text=self.affichageChemin, width =25)
+		self.usr_input .grid(row=1,column=1, sticky="EW")
 		
 		#Création et placement du Bouton ouvrir dossier
-		self.UF = tkinter.Button(self, text="Choisir", bg = "white", font = self.texte,bd=1, width = largeurBout-5,command = lambda:[self.Browser()])
-		self.UF.grid(row = 1, column = 2, sticky="E")
+		self.UF = tkinter.Button(self, text="Sélection Dossier", bg = "white", font = self.texte,bd=1,command = lambda:[self.Browser()])
+		self.UF.grid(row = 1, column = 0, sticky="W")
 		
 		#Création et placement du label Nombre de morceaux
 		tkinter.Label(self, text="Nombre de morceaux",height = hauteurBout, font = self.texte, bg='white').grid(row = 2, column =0, sticky="W")
 		#Création et placement d'une spinbox pour choisir le nombre de morceaux
-		self.nbMorceaux = tkinter.Spinbox(self, from_=1, to=20)
-		self.nbMorceaux.grid(row = 2, column =1, sticky="EW")
-
+		self.nbMorceaux = tkinter.Spinbox(self, from_=1, to=20, width=10)
+		self.nbMorceaux.grid(row = 2, column =1, sticky="W")
+		tkinter.Label(self, text="Entre 1-20   ", bg="white").grid(row=2, column=1, sticky="e")
+		
 		#Création et placement du label Durée de morceaux
 		tkinter.Label(self, text="Durée de morceaux",height = hauteurBout,  font = self.texte, bg='white').grid(row = 3, column =0, sticky="W")
 		
 		#Tonalité de morceaux
 		tkinter.Label(self, text="Tonalité de morceaux",height = hauteurBout,  font = self.texte, bg='white').grid(row = 4, column =0, sticky="W")
 		#Bouton Tonalite du morceau
-		self.tonaliteMorceau = tkinter.Spinbox(self, values = ("A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"))
-		self.tonaliteMorceau.grid(row = 4, column =1, sticky="EW")
+		self.tonaliteMorceau = tkinter.Spinbox(self, values = ("A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"), width=10)
+		self.tonaliteMorceau.grid(row = 4, column =1, sticky="W")
 		
 		#bpm des morceaux
 		tkinter.Label(self, text="Vitesse des morceaux",height = hauteurBout,  font = self.texte, bg='white').grid(row = 5, column =0, sticky="W")
 		#Bouton bpm des morceau
-		self.bpmMorceau = tkinter.Spinbox(self, from_=30,to=240)
-		self.bpmMorceau.grid(row = 5, column =1, sticky="EW")
+		self.bpmMorceau = tkinter.Spinbox(self, from_=30,to=240, width=10)
+		self.bpmMorceau.grid(row = 5, column =1, sticky="W")
+		tkinter.Label(self, text="Entre 30-240", bg="white").grid(row=5, column=1, sticky="e")
 		
 		#Choix de génération
 		tkinter.Label(self, text="Type de génération", width = 15, height = hauteurBout, font = self.texte, bg='white').grid(row = 6, column =0, sticky="W")
 				
 		#Création du spinbox de durée du morceau
-		self.dureeMorceau = tkinter.Spinbox(self, from_=60,to=340)
+		self.dureeMorceau = tkinter.Spinbox(self, from_=60,to=340, width=10)
 		#Placement de la spinbox
-		self.dureeMorceau.grid(row = 3, column =1, sticky="EW")
+		self.dureeMorceau.grid(row = 3, column =1, sticky="W")
+		tkinter.Label(self, text="Entre 60-340", bg="white").grid(row=3, column=1, sticky="e")
 		
 		#Création et placement du Bouton valider
 		self.textBoutonValider = StringVar()
 		self.textBoutonValider.set("Valider")
-		self.Valider = tkinter.Button(self, textvariable=self.textBoutonValider, bg="white", font = self.texte, bd=1, command=lambda :[self.charging(),self.export(),script.main(),master.switch_frame(lecteur.Lecteur)])
+		self.Valider = tkinter.Button(self, textvariable=self.textBoutonValider, bg="white", font = self.texte, bd=1, command=lambda :[self.charging(master)])
 		self.Valider.grid(row=7,column = 1,sticky="EW")
 		
 		
@@ -127,16 +132,48 @@ class Menu(tkinter.Frame):
 		#Réglage de l'item actuel sur 0
 		self.comboboite.current(0)
 		#Placement
-		self.comboboite.grid(row=6, column=1)
+		self.comboboite.grid(row=6, column=1, sticky="W")
 		
-		
+	def traitementAffichage(self, chemin, taille):
+		if len(chemin) > taille:
+			cheminCoupe = chemin[-(len(chemin)-taille):]
+			return cheminCoupe
+		else:
+			return chemin
+	
+	def valide(self):
+		valide = True
+		if(int(self.nbMorceaux.get())>20 or int(self.nbMorceaux.get())<1):
+			valide = False
+			#self.valide.set(1)
+		if(int(self.dureeMorceau.get())<60 or int(self.dureeMorceau.get())>340) :
+			valide = False
+			#self.dureeMorceau.set(60),
+		if(int(self.bpmMorceau.get()) <30 or int(self.bpmMorceau.get())>240) :
+			valide=False
+			#self.bpmMorceau.set(30)
+		if(not valide):
+			self.popupmsg()
+		return valide
 
-	def charging(self):
-		self.textBoutonValider.set("En chargement...")
-		#self.chargement = PhotoImage(file="./files/gif/charging.gif", format="gif -index 2")
-		#self.imageChargement = tkinter.Label(self, text="chargement")
-		#self.imageChargement.grid(row=5, column=0)
-		self.update()
+	def popupmsg(self):
+		popup = tkinter.Tk()
+		centrefenetre(popup)
+		popup.title("Erreur")
+		popup.config(bg="white")
+		label = tkinter.Label(popup, text="Attention : Le nombre de moreaux, ou \nla durée, ou les bpm est incorrect", bg="white")
+		popup.geometry("315x120")
+		label.pack(side="top", fill="x", pady=10)
+		ok = tkinter.Button(popup, text="Ok", bg="white", command = popup.destroy, width=10)
+		ok.pack()
+		popup.mainloop()
+
+	def charging(self, master):
+		if(self.valide()):
+			self.textBoutonValider.set("En chargement...")
+			self.export()
+			script.main()
+			master.switch_frame(lecteur.Lecteur)
 		return
 		
 
@@ -156,6 +193,7 @@ class Menu(tkinter.Frame):
 	def Browser(self):
 		filename = filedialog.askdirectory(initialdir = "./project2020-2021/files/midi/")
 		print("Filename is "+filename)
+		self.affichageChemin.set(self.traitementAffichage(filename,20))
 		self.entry_text.set(filename)
 			
 ####################################################### 
