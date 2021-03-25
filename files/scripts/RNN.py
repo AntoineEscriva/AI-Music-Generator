7280bbc5c2d214814b7fbc14a52f0db8f9a6b399
AntoineEscriva <antoine.20004@gmail.com>
2021-03-25 17:55:01 +0100

    Update extraire,RNN,script
    Gestion des paramètres de l'utilisateur (type de génération)

--- files/scripts/RNN.py
+++ files/scripts/RNN.py
@@ -103,24 +103,158 @@ def sample(model, out_len, start):
     return ''.join(chars)
 
 
-
-def rnn(txt):
-	#########################
-	# Debut premier section
-	#########################
-
-
+def device_choice():
 	# torch.cuda.is_available() checks and returns a Boolean True if a GPU is available, else it'll return False
 	is_cuda = torch.cuda.is_available()
 
 	# If we have a GPU available, we'll set our device to GPU. We'll use this device variable later in our code.
-	global device, int2char, char2int, dict_size
+	
 	if is_cuda:
 		device = torch.device("cuda")
 		print("GPU is available")
 	else:
 		device = torch.device("cpu")
 		print("GPU not available, CPU used")
+	return device
+
+
+# Partie RNN pour le rythme seulement
+
+
+def rnn_rythme(txt):
+	global device, int2char, char2int, dict_size
+
+	device = device_choice() #choix du device (CPU ou GPU)
+
+
+	taille = 10 #length of a sequence # de 100 à 200 généralement (50 pk pas)
+	batch_len = 16 # 16 ou 32
+	id_last_car = len(txt)-taille #index of the last character to parse for creating the sequences
+	text = []
+
+
+	for i in range(id_last_car):
+		text.append(txt[i:i+taille+1])
+
+
+	# Join all the sentences together and extract the unique characters from the combined sentences
+	chars = set(''.join(text))
+
+	# Creating a dictionary that maps integers to the characters
+	int2char = dict(enumerate(chars))
+
+	# Creating another dictionary that maps characters to integers
+	char2int = {char: ind for ind, char in int2char.items()}
+
+
+	# Finding the length of the longest string in our data
+	maxlen = len(max(text, key=len))
+
+
+
+	# Creating lists that will hold our input and target sequences
+	input_seq = []
+	target_seq = []
+
+	for i in range(len(text)):
+		# Remove last character for input sequence
+	  input_seq.append(text[i][:-1])
+		
+		# Remove first character for target sequence
+	  target_seq.append(text[i][1:])
+	  #print("Input Sequence: {}\nTarget Sequence: {}".format(input_seq[i], target_seq[i]))
+
+
+	for i in range(len(text)):
+		input_seq[i] = [char2int[character] for character in input_seq[i]]
+		target_seq[i] = [char2int[character] for character in target_seq[i]]
+
+
+	dict_size = len(char2int)
+	seq_len = maxlen - 1
+	batch_size = len(text)
+
+	##################
+	#Fin premiere section
+	##################
+
+
+
+	###################################
+	# Debut 2e section
+	###################################
+	# Input shape --> (Batch Size, Sequence Length, One-Hot Encoding Size)
+	input_seq = one_hot_encode(input_seq, dict_size, seq_len, batch_size)
+
+	input_seq = torch.from_numpy(input_seq)
+	target_seq = torch.Tensor(target_seq)
+	###################################
+	# Fin 2e section
+	###################################
+
+
+
+
+
+
+
+	# Instantiate the model with hyperparameters
+	model = Model(input_size=dict_size, output_size=dict_size, hidden_dim=512, n_layers=1)
+	# We'll also set the model to the device that we defined earlier
+	model = model.to(device)
+
+	# set the input_seq ant target_seq to the device used
+	input_seq = input_seq.to(device) 
+	target_seq = target_seq.to(device)
+
+
+	# Define hyperparameters
+	n_epochs = 50
+	lr=0.001
+
+
+	# Define Loss, Optimizer
+	criterion = nn.CrossEntropyLoss()
+	optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+	print("Début de l'Entraînement")
+
+	# Training Run
+	t1 = time.time()
+	for epoch in range(1, n_epochs):
+		optimizer.zero_grad() # Clears existing gradients from previous epoch
+		#input_sample, target_sample = sample_seq(batch_len, batch_size, input_seq, target_seq)
+		output, hidden = model(input_seq)
+		loss = criterion(output, target_seq.view(-1).long())
+
+		loss.backward() # Does backpropagation and calculates gradients
+		optimizer.step() # Updates the weights accordingly
+		
+		if epoch%1 == 0:
+			t2 = time.time()-t1
+			t1 = time.time()
+			print('Epoch: {}/{}.............'.format(epoch, n_epochs), end=' ')
+			print("Loss: {:.4f}   {:.4f}".format(loss.item(), t2))
+
+        # ICI ON RECUPERE LES PARAMETRES CONCERNANT LE NOMBRE DE MORCEAUX, LA DUREE, ETC
+	
+	print("Entraînement fini, génération des morceaux")
+
+	out = [sample(model, 200, "t")] # on retourne le résultat sous la forme d'une liste
+
+	print(out)
+
+	# /!\ Attention, il faut couper la sortie en enlevant les triplets non complets !!
+
+	return out
+
+
+# Partie RNN pour le rythme et la mélodie
+
+def rnn_rythme_melodie(txt):
+	global device, int2char, char2int, dict_size
+
+	device = device_choice() #choix du device (CPU ou GPU)
 
 
 	taille = 10 #length of a sequence # de 100 à 200 généralement (50 pk pas)
--- files/scripts/script.py
+++ files/scripts/script.py
@@ -5,33 +5,62 @@ from files.scripts import RNN as RNN
 from files.scripts import importExportParam as iep 
 from files.scripts import extraire as extraire
 from os import walk
+import os
+
 
 def main():
 	print("Je suis un script")
 	parametres = iep.importFromCSV()
+
+	os.makedirs(parametres["URL_Dossier"]+'/'+"CSV",exist_ok=True)
+	os.makedirs(parametres["URL_Dossier"]+'/'+"Conversion",exist_ok=True)
+	os.makedirs(parametres["URL_Dossier"]+'/'+"Resultat",exist_ok=True)
 	
+	#on récupère tous les fichiers du dossier
 	listeFichiers = []
 	for (repertoire, sousRepertoires, fichiers) in walk(parametres["URL_Dossier"]):
 		listeFichiers.extend(fichiers)
-	print(listeFichiers)
 	listeFichiers = [i for i in listeFichiers if ".mid" in i]
+	print (listeFichiers)
+
+	#on récupère tous les fichiers du dossier /Conversion pour ne pas avoir à reconvertir des fichiers
+	listeFichiersConvert = []
+	for (repertoire, sousRepertoires, fichiers) in walk(parametres["URL_Dossier"]+'/'+"Conversion"):
+		listeFichiersConvert.extend(fichiers)
+	print(listeFichiersConvert)
 	
+	#on tranforme les fichiers midi en objet Morceau
 	listeMorceaux = [] # liste d'objets de type Morceau
 	for files in listeFichiers:
 		listeMorceaux.append(extraire.Morceau(parametres["URL_Dossier"]+'/'+files))
-
 	print(listeMorceaux[0].filename)
+	
+	
 	## A CHANGER ##
+
+	#on prépare les morceaux pour le RNN
 	liste_textes = []
 	for m in listeMorceaux:
-		if(m.format == 1 and m.nbTracks > 1):
-			liste_textes.append(m.preparer_track(2)) #on récupère la piste 2 du premier morceau qu'on trouve
+		if(m.format == 1 and m.nbTracks > 1 ):
+			if (parametres["TypeGeneration"] == "Rythme seulement"):
+				liste_textes.append(m.preparer_track_rythme(2)) #on récupère la piste 2 du premier morceau qu'on trouve
+			if (parametres["TypeGeneration"] == "Rythme et mélodie"):
+				liste_textes.append(m.preparer_track(2)) #on récupère la piste 2 du premier morceau qu'on trouve
 	
-	out = RNN.rnn(liste_textes[0]) #on envoie au RNN et on récupère la sortie
+	#print(liste_textes)
+	
+	if (parametres["TypeGeneration"] == "Rythme seulement"):
+		out = RNN.rnn_rythme(liste_textes[0]) #on envoie au RNN et on récupère la sortie
+	elif (parametres["TypeGeneration"] == "Rythme et mélodie"):
+		out = RNN.rnn_rythme_melodie(liste_textes[0])
 
 	for index in range(len(out)):
-		listeMorceaux[index].format_to_csv(out[index], listeMorceaux[index].filename.replace(".csv", "-généré.mid")) # enregistre le morceau sous format MIDI
+		if (parametres["TypeGeneration"] == "Rythme seulement"):
+			listeMorceaux[index].format_to_csv_rythme(out[index]) # enregistre le morceau sous format MIDI
+		elif (parametres["TypeGeneration"] == "Rythme et mélodie"):
+			listeMorceaux[index].format_to_csv(out[index]) # enregistre le morceau sous format MIDI
 
-	
+
 if __name__ == "__main__":
 	main()
+	
