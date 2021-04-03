b0d7d5fffe07ccf25c1c82e079bec07de3669ae7
Karatsuban <75843468+Karatsuban@users.noreply.github.com>
2021-04-03 15:49:50 +0200

    Add files via upload

---
+++ source/controller/ImportExportParametres.py
@@ -0,0 +1,43 @@
+# !/usr/bin/python3
+#coding: utf-8
+
+import csv
+import os
+
+def export(parametres):
+	fichier = open("data"+os.sep+"parametres.txt", 'w')
+	
+	fichier.write(str(parametres))
+	
+	fichier.close()
+	print("Fichier de paramètres exporté")
+	return
+	
+	
+def exportInCSV(parametres):
+	with open("data"+os.sep+"parametres.csv", 'w') as fichier :
+		w = csv.DictWriter(fichier, parametres.keys())
+		w.writeheader()
+		w.writerow(parametres)
+	return
+	
+def importFromCSV():
+	with open("data"+os.sep+"parametres.csv", newline='') as csvfile:
+		reader = csv.DictReader(csvfile)
+		parametres = {}
+		for row in reader:
+			parametres['URL_Dossier'] = row['URL_Dossier']
+			parametres['NombreMorceaux'] = row['NombreMorceaux']
+			parametres['DureeMorceaux']= row['DureeMorceaux']
+			parametres['TonaliteMorceaux']= row['TonaliteMorceaux']
+			parametres['VitesseMorceaux']= row['VitesseMorceaux']
+			parametres['TypeGeneration'] = row['TypeGeneration']
+	return parametres
+	
+def getURL():
+	with open("data"+os.sep+"parametres.csv", newline='') as csvfile:
+		reader = csv.DictReader(csvfile)
+		parametres = {}
+		for row in reader:
+			return row['URL_Dossier']
+			 
