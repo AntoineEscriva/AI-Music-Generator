7810e3315eb625ecc4a99d3d8ce7925d1f48fc67
Karatsuban <75843468+Karatsuban@users.noreply.github.com>
2021-04-07 21:16:20 +0200

    Update RNN.py
    Ajouté (enfin) la vectorisation pour rnn_rythme_melodie, c'est à dire le fait que les triplets sont interprétes comme une seule information (une note)

--- source/model/RNN.py
+++ source/model/RNN.py
@@ -10,7 +10,19 @@ import random
 from matplotlib import pyplot as plt
 
 
-def one_hot_encode(sequence, dict_size, seq_len, batch_size):
+
+def triplets_to_notes(entree):
+  sortie = []
+  for a in entree:  
+    notes = a.strip().split(" ") #on sépare les triplets (en enlevant les potentiels espaces de fin)
+    notes = [note.split(":") for note in notes] #on éclate les triplets
+    notes = [ [int(n[0]), int(n[1]), int(n[2])] for n in notes] #on caste les informations tirées des triplets en int
+    sortie.append(notes)
+  return sortie
+
+
+
+def one_hot_encode_rythme(sequence, dict_size, seq_len, batch_size):
     # Creating a multi-dimensional array of zeros with the desired output shape
     features = np.zeros((batch_size, seq_len, dict_size), dtype=np.float32)
     
@@ -21,8 +33,17 @@ def one_hot_encode(sequence, dict_size, seq_len, batch_size):
     return features
 
 
+def one_hot_encode_melodie(sequence, dict_size, seq_len, batch_size):
+  # sequence est un triplet d'entiers
+    features = np.zeros((batch_size, seq_len, dict_size), dtype=np.float32)
+
+    for i in range(batch_size):
+        for u in range(seq_len):
+            features[i, u] = np.array(sequence[i][u])
+    return features
+
 #return nb_samples samples from input_t & target_t
-def sample_seq(nb_samples, total, input_t, target_t):
+def sample_seq_rythme(nb_samples, total, input_t, target_t):
     indexes = np.random.randint(0, total-1, nb_samples)
     input_shape = input_t.size()
     target_shape = target_t.size()
@@ -34,6 +55,19 @@ def sample_seq(nb_samples, total, input_t, target_t):
         new_target[a] = target_t[indexes[a]].detach().clone()
     return new_input.to(device), new_target.to(device)
 
+
+#return nb_samples samples from input_t & target_t
+def sample_seq_melodie(nb_samples, total, input_t, target_t):
+    indexes = np.random.randint(0, total-1, nb_samples)
+    input_shape = input_t.size()
+    target_shape = target_t.size()
+    new_input = torch.randn((nb_samples, input_shape[1], input_shape[2]), dtype=torch.float32)
+    new_target = torch.randn((nb_samples, target_shape[1], input_shape[2]), dtype=torch.float32)
+
+    for a in range(len(indexes)):
+        new_input[a] = input_t[indexes[a]].detach().clone()
+        new_target[a] = target_t[indexes[a]].detach().clone()
+    return new_input.to(device), new_target.to(device)
 
 
 class Model(nn.Module):
@@ -74,10 +108,10 @@ class Model(nn.Module):
 
 
 # This function takes in the model and character as arguments and returns the next character prediction and hidden state
-def predict(model, character):
+def predict_rythme(model, character):
     # One-hot encoding our input to fit into the model
     character = np.array([[char2int[c] for c in character]])
-    character = one_hot_encode(character, dict_size, character.shape[1], 1)
+    character = one_hot_encode_rythme(character, dict_size, character.shape[1], 1)
     character = torch.from_numpy(character)
     character = character.to(device)
     
@@ -91,7 +125,7 @@ def predict(model, character):
 
 
 # This function takes the desired output length and input characters as arguments, returning the produced sentence
-def sample(model, out_len, start):
+def sample_rythme(model, out_len, start):
     model.eval() # eval mode
     ## start = start.lower() # WHY ?
     # First off, run through the starting characters
@@ -99,10 +133,40 @@ def sample(model, out_len, start):
     size = out_len - len(chars)
     # Now pass in the previous characters and get a new one
     for ii in range(size):
-        char, h = predict(model, chars)
+        char, h = predict_rythme(model, chars)
         chars.append(char)
 
     return ''.join(chars)
+
+
+# This function takes in the model and character as arguments and returns the next character prediction and hidden state
+def predict_melodie(model, character):
+    # One-hot encoding our input to fit into the model
+    character = triplets_to_notes(character)[0]
+    character = one_hot_encode(character, dict_size, len(character), 1)
+    character = torch.from_numpy(character)
+    character = character.to(device)
+    
+    out, hidden = model(character)
+    
+    new_vec = out.detach().to('cpu').numpy()[0]
+    new_vec = ":".join([str(int(elt)) for elt in new_vec])
+
+    return new_vec, hidden
+
+# This function takes the desired output length and input characters as arguments, returning the produced sentence
+def sample_melodie(model, out_len, start):
+    model.eval() # eval mode
+    ## start = start.lower() # WHY ?
+    # First off, run through the starting characters
+    chars = [ch for ch in start]
+    size = out_len - len(chars)
+    # Now pass in the previous characters and get a new one
+    for ii in range(size):
+        char, h = predict_rythme(model, chars)
+        chars.append(char)
+
+    return ' '.join(chars)
 
 
 def device_choice():
@@ -215,13 +279,13 @@ def rnn_rythme(input_list):
 
 	# Input shape --> (Batch Size, Sequence Length, One-Hot Encoding Size)
 	#encodage pour training
-	training_input_seq = one_hot_encode(training_input_seq, dict_size, seq_len, training_batch_size)
+	training_input_seq = one_hot_encode_rythme(training_input_seq, dict_size, seq_len, training_batch_size)
 
 	training_input_seq = torch.from_numpy(training_input_seq)
 	training_target_seq = torch.Tensor(training_target_seq)
 
 	# idem pour test
-	test_input_seq = one_hot_encode(test_input_seq, dict_size, seq_len, test_batch_size)
+	test_input_seq = one_hot_encode_rythme(test_input_seq, dict_size, seq_len, test_batch_size)
 
 	test_input_seq = torch.from_numpy(test_input_seq)
 	test_target_seq = torch.Tensor(test_target_seq)
@@ -264,7 +328,7 @@ def rnn_rythme(input_list):
 	for epoch in range(1, n_epochs):
 		optimizer.zero_grad() # Clears existing gradients from previous epoch
 		if (is_batch):
-			training_input_sample, training_target_sample = sample_seq(batch_len, training_batch_size, training_input_seq, training_target_seq)
+			training_input_sample, training_target_sample = sample_seq_rythme(batch_len, training_batch_size, training_input_seq, training_target_seq)
 			output, hidden = model(training_input_sample)
 			new_training_loss = criterion(output, training_target_sample.view(-1).long())
 		else:
@@ -281,7 +345,7 @@ def rnn_rythme(input_list):
 			print("Loss: {:.4f}   {:.4f}".format(new_training_loss.item(), t2))
 
 			if(is_batch):
-				test_input_sample, test_target_sample = sample_seq(batch_len, test_batch_size, test_input_seq, test_target_seq)
+				test_input_sample, test_target_sample = sample_seq_rythme(batch_len, test_batch_size, test_input_seq, test_target_seq)
 				output, hidden = model(test_input_sample)
 				new_test_loss = criterion(output, test_target_sample.view(-1).long())
 			else:
@@ -322,7 +386,7 @@ def rnn_rythme(input_list):
 	plt.plot(x,y3, color='g') #vert
 	#plt.show()
 
-	out = [sample(model, 100, int2char[random.randint(0,len(int2char)-1)]) for i in range(3)] # on retourne le résultat sous la forme d'une liste
+	out = [sample_rythme(model, 100, int2char[random.randint(0,len(int2char)-1)]) for i in range(3)] # on retourne le résultat sous la forme d'une liste
 
 	print(out)
 
@@ -334,129 +398,153 @@ def rnn_rythme(input_list):
 
 # Partie RNN pour le rythme et la mélodie
 
-def rnn_rythme_melodie(txt):
+def rnn_rythme_melodie(input_list):
 	global device, int2char, char2int, dict_size
+
+	input_list = triplets_to_notes(input_list) #on transforme la chaîne de triplets en liste de triplets
 
 	device = device_choice() #choix du device (CPU ou GPU)
 
+	taille = 200 #longueur d'une séquence # de 100 à 200 généralement
+	batch_len = 8 # 16 ou 32
+	is_batch = True
 
-	taille = 10 #length of a sequence # de 100 à 200 généralement (50 pk pas)
-	batch_len = 16 # 16 ou 32
-	id_last_car = len(txt)-taille #index of the last character to parse for creating the sequences
-	text = []
+	training_text = [] # liste des training files de longueur taille+1 que l'on va découper
+	test_text = [] # liste des tests files de longueur taille+1 que l'on va découper 
 
-
-	for i in range(id_last_car):
-		text.append(txt[i:i+taille+1])
-
-
-	# Join all the sentences together and extract the unique characters from the combined sentences
-	chars = set(''.join(text))
-
-	# Creating a dictionary that maps integers to the characters
-	int2char = dict(enumerate(chars))
-
-	# Creating another dictionary that maps characters to integers
-	char2int = {char: ind for ind, char in int2char.items()}
+	nb_test_files = testfile_number_choice(len(input_list))
+	training_files, test_files = testfile_choice(input_list, nb_test_files)
 
 
-	# Finding the length of the longest string in our data
-	maxlen = len(max(text, key=len))
+	for i in training_files:
+		training_text += decoupe_morceau(i, taille)
+	
 
+	for i in test_files:
+		test_text += decoupe_morceau(i, taille)
 
-
+	
 	# Creating lists that will hold our input and target sequences
-	input_seq = []
-	target_seq = []
+	training_input_seq = []
+	training_target_seq = []
+	test_input_seq = []
+	test_target_seq = []
 
-	for i in range(len(text)):
+
+	for i in range(len(training_text)):
 		# Remove last character for input sequence
-	  input_seq.append(text[i][:-1])
-		
+		training_input_seq.append(training_text[i][:-1])
+
 		# Remove first character for target sequence
-	  target_seq.append(text[i][1:])
-	  #print("Input Sequence: {}\nTarget Sequence: {}".format(input_seq[i], target_seq[i]))
+		training_target_seq.append(training_text[i][1:])
 
+	for i in range(len(test_text)):
+		# Remove last character for input sequence
+		test_input_seq.append(test_text[i][:-1])
 
-	for i in range(len(text)):
-		input_seq[i] = [char2int[character] for character in input_seq[i]]
-		target_seq[i] = [char2int[character] for character in target_seq[i]]
-
-
-	dict_size = len(char2int)
-	seq_len = maxlen - 1
-	batch_size = len(text)
-
-	##################
-	#Fin premiere section
-	##################
+		# Remove first character for target sequence
+		test_target_seq.append(test_text[i][1:])
 
 
 
-	###################################
-	# Debut 2e section
-	###################################
+	dict_size = 3 #constante
+	seq_len = taille
+	training_batch_size = len(training_text)
+	test_batch_size = len(test_text)
+
 	# Input shape --> (Batch Size, Sequence Length, One-Hot Encoding Size)
-	input_seq = one_hot_encode(input_seq, dict_size, seq_len, batch_size)
+	#encodage pour training
 
-	input_seq = torch.from_numpy(input_seq)
-	target_seq = torch.Tensor(target_seq)
-	###################################
-	# Fin 2e section
-	###################################
+	training_input_seq = one_hot_encode_melodie(training_input_seq, dict_size, seq_len, training_batch_size)
 
+	training_input_seq = torch.from_numpy(training_input_seq)
+	training_target_seq = torch.Tensor(training_target_seq)
 
+	# idem pour test
+	test_input_seq = one_hot_encode_melodie(test_input_seq, dict_size, seq_len, test_batch_size)
 
-
-
+	test_input_seq = torch.from_numpy(test_input_seq)
+	test_target_seq = torch.Tensor(test_target_seq)
 
 
 	# Instantiate the model with hyperparameters
-	model = Model(input_size=dict_size, output_size=dict_size, hidden_dim=512, n_layers=1)
+	model = Model(input_size=dict_size, output_size=dict_size, hidden_dim=512, n_layers=2)
 	# We'll also set the model to the device that we defined earlier
 	model = model.to(device)
 
-	# set the input_seq ant target_seq to the device used
-	input_seq = input_seq.to(device) 
-	target_seq = target_seq.to(device)
+	# set the training_input_seq ant training_target_seq to the device used
+	training_input_seq = training_input_seq.to(device) 
+	training_target_seq = training_target_seq.to(device)
 
+	#idem pour les test
+	test_input_seq = test_input_seq.to(device) 
+	test_target_seq = test_target_seq.to(device)
 
 	# Define hyperparameters
-	n_epochs = 50
-	lr=0.001
+	n_epochs = 100
+	lr=0.01
 
 
 	# Define Loss, Optimizer
-	criterion = nn.CrossEntropyLoss()
+	#criterion = nn.CrossEntropyLoss()
+	criterion = nn.L1Loss()
 	optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
 	print("Début de l'Entraînement")
+
+	old_training_loss = 100
+	old_test_loss = 100
+	list_training_loss = []
+	list_test_loss = []
+	list_lr = []
 
 	# Training Run
 	t1 = time.time()
 	for epoch in range(1, n_epochs):
 		optimizer.zero_grad() # Clears existing gradients from previous epoch
-		#input_sample, target_sample = sample_seq(batch_len, batch_size, input_seq, target_seq)
-		output, hidden = model(input_seq)
-		loss = criterion(output, target_seq.view(-1).long())
+		if (is_batch):
+			training_input_sample, training_target_sample = sample_seq_melodie(batch_len, training_batch_size, training_input_seq, training_target_seq)
+			output, hidden = model(training_input_sample)
+			new_training_loss = criterion(output.view(-1), training_target_sample.view(-1).long())
+		else:
+			output, hidden = model(training_input_seq)
+			new_training_loss = criterion(output.veiw(-1), training_target_seq.view(-1).long())
 
-		loss.backward() # Does backpropagation and calculates gradients
+		new_training_loss.backward() # Does backpropagation and calculates gradients
 		optimizer.step() # Updates the weights accordingly
 		
-		if epoch%1 == 0:
+		if epoch%10 == 0:
 			t2 = time.time()-t1
 			t1 = time.time()
 			print('Epoch: {}/{}.............'.format(epoch, n_epochs), end=' ')
-			print("Loss: {:.4f}   {:.4f}".format(loss.item(), t2))
+			print("Loss: {:.4f}   {:.4f}".format(new_training_loss.item(), t2))
 
-        # ICI ON RECUPERE LES PARAMETRES CONCERNANT LE NOMBRE DE MORCEAUX, LA DUREE, ETC
+			if(is_batch):
+				test_input_sample, test_target_sample = sample_seq_melodie(batch_len, test_batch_size, test_input_seq, test_target_seq)
+				output, hidden = model(test_input_sample)
+				new_test_loss = criterion(output.view(-1), test_target_sample.view(-1).long())
+			else:
+				output, hidden = model(test_input_seq)
+				new_test_loss = criterion(output.view(-1), test_target_seq.view(-1).long())
+
+			if(new_test_loss < old_test_loss):
+				print("Test sur les données de test \t Loss : {} BAISSE".format(new_test_loss.item()))
+			else:
+				print("Test sur les données de test \t Loss : {} AUGMENTATION".format(new_test_loss.item()))
+
+			old_test_loss = new_test_loss
+			list_test_loss.append(new_test_loss)
+
+		old_training_loss = new_training_loss
+		list_training_loss.append(new_training_loss)
+		list_lr.append(lr)
+		#lr -= (1/100) * lr #mise à jour du learning rate
 	
 	print("Entraînement fini, génération des morceaux")
 
-	out = [sample(model, 200, "360:p:76 ")] # on retourne le résultat sous la forme d'une liste
+	out = [sample_melodie(model, 100, ["480:120:76"])]
+	#out = [sample(model, 100, int2char[random.randint(0,len(int2char)-1)]) for i in range(3)] # on retourne le résultat sous la forme d'une liste
 
 	print(out)
-
-	# /!\ Attention, il faut couper la sortie en enlevant les triplets non complets !!
 
 	return out
