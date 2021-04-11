b15d341186729d76c13b6b0ab48c586dc59d3465
Karatsuban <75843468+Karatsuban@users.noreply.github.com>
2021-04-11 12:17:54 +0200

    Delete MusicRNNLauncher.py

--- MusicRNNLauncher.py
+++
@@ -1,9 +0,0 @@
-
-# !/usr/bin/python3
-#coding: utf-8
-from source.view.scripts import InterfaceChoixParametres, InterfaceLecteur
-import Install
-
-if __name__ == "__main__":
-	Install.installRequiredPackages()
-	InterfaceChoixParametres.start()
